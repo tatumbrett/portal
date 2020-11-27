cc28255b78930cf5c8cc17caeb59f6000fbaca7c
tatumbrett <tatumbrett@users.noreply.github.com>
2020-11-27 14:08:40 -0600

    setup-changes

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ucw2R4Hcf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7l2vt2m37eu5hes16essmttr48', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://abcdefghijkl.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://lka8a30kl3.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'tatum.brett@gmail.com'
     }
 };
